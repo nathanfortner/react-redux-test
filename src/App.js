68594b90e51cd40062250a8f9a883b85d186388b
Nathan Fortner <nathan.fortner@wesayhi.com>
2020-07-18 20:49:17 -0400

    i love typing in vim

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
+import {decrement} from './actions';
+import {isLoggedIn} from './actions';
+
 
 function App() {
+  const counterReducer = useSelector(state => state.counterReducer);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {counterReducer}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(isLoggedIn())}>Log In</button>
+
+      {isLogged ? <h3>things that I should not see if not logged in</h3> : <h3>nothing to see here</h3>}
     </div>
   );
 }
